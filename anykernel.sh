e3850b4e63963c9eb9aeee9b108b3b76b52f0698
mahiru <108695555+Yuddciel@users.noreply.github.com>
2025-04-01 02:35:08 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Oxygen karnal by Mahiru
+kernel.string=Oplus karnal by Mahiru
 do.devicecheck=1
 do.modules=0
 do.systemless=1
