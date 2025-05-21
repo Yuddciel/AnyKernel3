538d38b90db374c0d21413316623a3ef5cecb590
mahiru <108695555+Yuddciel@users.noreply.github.com>
2025-05-22 04:44:08 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Miui Kernel by Mahiru
+kernel.string=HyperOS Kernel by Mahirooo
 do.devicecheck=1
 do.modules=0
 do.systemless=1
