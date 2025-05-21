cc12db6773ab04954226d04075cc55c78e185aeb
mahiru <108695555+Yuddciel@users.noreply.github.com>
2025-05-21 22:30:06 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Oplus karnal by Mahiru
+kernel.string=Miui Kernel by Mahiru
 do.devicecheck=1
 do.modules=0
 do.systemless=1
